a62f1ab7f191a2bd038e0eb14878a09913adec91
David LaFollette <65127816+ProjectManEdge@users.noreply.github.com>
2024-10-19 07:46:52 -0600

    Calendar tab mockup (#3)

--- app/src/main/java/com/example/mentalhealthtracker/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/example/mentalhealthtracker/ui/calendar/CalendarFragment.java
@@ -24,8 +24,6 @@ public class CalendarFragment extends Fragment {
         binding = FragmentCalendarBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textCalendar;
-        calendarViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
