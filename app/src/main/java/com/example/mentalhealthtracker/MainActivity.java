d75490cba4a4bed07df2375d339c35f6741a825e
ProjectManEdge <laf16003@byui.edu>
2024-10-19 16:10:33 -0600

    Disabled Home page due to being inoperable

--- app/src/main/java/com/example/mentalhealthtracker/MainActivity.java
+++ app/src/main/java/com/example/mentalhealthtracker/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_chat, R.id.navigation_calendar, R.id.navigation_help)
+                /*R.id.navigation_home,*/ R.id.navigation_chat, R.id.navigation_calendar, R.id.navigation_help)
                 .build();
 
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
