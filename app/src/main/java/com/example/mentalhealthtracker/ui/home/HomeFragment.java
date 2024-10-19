e0c78c9586421e41cb7748c724f1f9206590183f
ProjectManEdge <laf16003@byui.edu>
2024-10-19 16:24:02 -0600

    Added Calender listener to Home

--- app/src/main/java/com/example/mentalhealthtracker/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/mentalhealthtracker/ui/home/HomeFragment.java
@@ -1,9 +1,14 @@
 package com.example.mentalhealthtracker.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -23,6 +28,27 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        CalendarView calenderFeeling = binding.calendarFeeling; // Calendar
+        RatingBar feeling = binding.feelingRating;              // Daily Star Rating
+        TextView feelingBox = binding.textRecord;               // Text Box
+
+        // Setup page
+        calenderFeeling.setDate(System.currentTimeMillis());
+
+        // Start listeners
+
+        // Calender Date Change
+        calenderFeeling.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Log.d("Feeling-Calendar",
+                        "User set the date to: "
+                                + dayOfMonth + " / "
+                                + month + " / "
+                                + year);
+            }
+        });
 
         return root;
     }
